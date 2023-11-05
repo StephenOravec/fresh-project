f9225c3413bc6bd8b919ea90ac32355b88e5a63f
Stephen Oravec <stephen@oravec.io>
2023-11-05 12:43:31 -0500

    Customize tutorial boilerplate

--- routes/index.tsx
+++ routes/index.tsx
@@ -2,7 +2,7 @@ import { useSignal } from "@preact/signals";
 import Counter from "../islands/Counter.tsx";
 
 export default function Home() {
-  const count = useSignal(4);
+  const count = useSignal(69);
   return (
     <div class="px-4 py-8 mx-auto bg-[#86efac]">
       <div class="max-w-screen-md mx-auto flex flex-col items-center justify-center">
@@ -13,18 +13,17 @@ export default function Home() {
           height="128"
           alt="the Fresh logo: a sliced lemon dripping with juice"
         />
-        <h1 class="text-4xl font-bold">Welcome to Fresh</h1>
+        <h1 class="text-4xl font-bold">Hello, Fresh!</h1>
         <p class="my-4">
-          Try updating this message in the
-          <code class="mx-2">./routes/index.tsx</code> file, and refresh.
+          This being my first deployment with Deno & Fresh.
+        </p>
+        <p class="my-4">
+          You can make the number go up and down!
         </p>
         <Counter count={count} />
         <p class="font-serif text-blue-700">
-          Hello, World!
+          This is me playing with Tailwind.
         </p>
-      </div>
-      <div class="bg-[#bababa]">
-        <p class="text-red-700 text-right">HMMM</p>
       </div>
     </div>
 
